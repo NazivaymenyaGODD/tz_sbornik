9f6cc7e8d7e75964dde27378908ecec8d365206b
NazivaymenyaGODD <121785142+NazivaymenyaGODD@users.noreply.github.com>
2023-03-10 22:10:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,14 +14,13 @@ conn.autocommit = True
 # # #Создал БД
 
 cursor.execute(
-          """CREATE TABLE member(
-                  id varchar(30),
-                  complex varchar(50),
-                  subject varchar(80),
-                  step1 int,
-                  step2 int
-                )"""
-              )
+            """CREATE TABLE member(
+                    id varchar(30),
+                   complex varchar(50),
+                    subject varchar(80),
+                    step1 int
+                  )"""
+               )
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
@@ -29,17 +28,18 @@ def start_message(message):
     keyboard = telebot.types.ReplyKeyboardMarkup(True)
     keyboard.row('Выбрать сложность задач','Выбрать тематику задач', 'Получить задания')
     bot.send_message(message.chat.id, "Привет, я сборник задач. Первым делом выбирайте сложность и тематику задачи, потом вы можете получить задачи.", reply_markup=keyboard)
-    cursor.execute("SELECT id FROM member")
-    data = cursor.fetchall()
-    check = 0
-    for i in range(len(data)):
-        if data[i][0] == str(message.chat.id):
-            check = 1
-    if  check == 0: cursor.execute("INSERT INTO member (id, step1, step2) VALUES(%s, 2, 2);", [message.chat.id])
-
+    
 @bot.message_handler(content_types=['text'])
 def send_text(message):
-    
+    if message:
+        cursor.execute("SELECT id FROM member")
+        data = cursor.fetchall()
+        check = 0
+        for i in range(len(data)):
+            if data[i][0] == str(message.chat.id):
+                check = 1
+        if  check == 0: cursor.execute("INSERT INTO member (id, step1) VALUES(%s, 0);", [message.chat.id])
+
     if message.text == 'Выбрать сложность задач':
         cursor.execute("""SELECT DISTINCT complexity FROM task""")
         data = cursor.fetchall()
@@ -65,13 +65,11 @@ def send_text(message):
             text = text + str(data[i][0] + '\n')
         text = text + 'Введите тематику:'
         bot.send_message(message.chat.id, text)
-        cursor.execute("UPDATE member SET step2 = 1 WHERE id = %s;", [str(message.chat.id)])
+        cursor.execute("UPDATE member SET step1 = 2 WHERE id = %s;", [str(message.chat.id)])
         
     if message.text != 'Выбрать тематику задач' and message.text != 'Выбрать сложность задач':
         cursor.execute("SELECT step1 FROM member WHERE id = %s;", [str(message.chat.id)])
         data = cursor.fetchall()
-        cursor.execute("SELECT step2 FROM member WHERE id = %s;", [str(message.chat.id)])
-        data2 = cursor.fetchall()
         if data[0][0] == 1:
             cursor.execute("""SELECT DISTINCT complexity FROM task""")
             data = cursor.fetchall()
@@ -85,7 +83,7 @@ def send_text(message):
             if check == 0:
                 bot.send_message(message.chat.id, "Такой сложности нету. Попробуйте снова")
         
-        if data2[0][0] == 1:
+        if data[0][0] == 2:
             cursor.execute("""SELECT s1 FROM subject
                             UNION
                             SELECT s2 FROM subject
@@ -99,7 +97,7 @@ def send_text(message):
                 if data[i][0] == message.text:
                     check = 1
             if check == 1:
-                cursor.execute("UPDATE member SET step2 = 0 WHERE id = %s;", [str(message.chat.id)])
+                cursor.execute("UPDATE member SET step1 = 0 WHERE id = %s;", [str(message.chat.id)])
                 cursor.execute("UPDATE member SET subject = %s WHERE id = %s;", [message.text, str(message.chat.id)])
             if check == 0:
                 bot.send_message(message.chat.id, "Такой тематики нету. Попробуйте снова")        
@@ -119,9 +117,12 @@ def send_text(message):
                 text = ''
                 for i in range(len(data)):
                     text = text +  data[i][0] + "/" + data[i][1] + ". https://codeforces.com/" + data[i][2] + '\n'
-                bot.send_message(message.chat.id, text)
+                if text != '':
+                    bot.send_message(message.chat.id, text)
+                else:
+                    bot.send_message(message.chat.id, 'такой подборки не нашлось')
             else:
-                bot.send_message(message.chat.id, "Не выбрано сложность или темматика задачи.")
+                bot.send_message(message.chat.id, "Не выбрано сложность или тематика задачи.")
 
 
 bot.polling()
